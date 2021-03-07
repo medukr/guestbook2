abb1d41c4cbe421373e710c379e5eedbaa17234b
medukr <andrii.demydyuk@gmail.com>
2021-03-07 19:26:46 +0200

    10.4

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,5 +23,18 @@ class ConferenceController extends AbstractController
             'conferences' => $conferenceRepository->findAll()
         ]));
 
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy([
+                'conference' => $conference,
+            ], [ 'createdAt' => 'DESC'])
+        ]));
     }
 }
