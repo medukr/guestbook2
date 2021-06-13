bef941af0741e6a46b93d13647c81ca3db68ca7b
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-06-13 19:56:05 +0300

    21.6

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -14,12 +14,28 @@ class StepInfoCommand extends Command
 {
     protected static $defaultName = 'app:step:info';
 
+    private $cache;
+
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+
+        parent::__construct();
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
-        $process->mustRun();
-        $output->write($process->getOutput());
+
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+
+            $item->expiresAfter(30);
+
+            return $process->getOutput();
+        });
+
+        $output->writeln($step);
 
         return Command::SUCCESS;
     }
