57fd84bb30581851b3d19f1debe9f1b0dd9b12d9
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-07-03 19:02:10 +0300

    27.6

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,21 +1,46 @@
+import '../assets/css/app.scss';
+
 import { h, render } from 'preact';
 import { Link, Router } from 'preact-router';
+import { useState, useEffect } from "preact/hooks";
 
+import { findConferences} from "./api/api";
 import Home from "./pages/home";
 import Conference from "./pages/conference";
 
 function App() {
+    const [conferences, setConferences] = useState(null)
+
+    useEffect(() => {
+        findConferences().then((conferences) => setConferences(conferences))
+    }, [])
+
+    if (conferences == null) {
+        return <div className="text-center pt-5">Loading...</div>
+    }
+
     return (
         <div>
-            <header>
-                <Link href="/">Home</Link>
-                <br/>
-                <Link href="/conference/amsterdam2019">Amsterdam2019</Link>
+            <header className="header">
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container">
+                        <Link className="navbar-brand mr-4 pr-2" href="/">
+                            &#128217; Guestbook
+                        </Link>
+                    </div>
+                </nav>
+                <nav className="bg-light border-bottom text-center">
+                    {conferences.map((conference) => (
+                            <Link className="nav-conference" href={'/conference/'+conference.slug}>
+                                {conference.city} {conference.year}
+                            </Link>
+                    ))}
+                </nav>
             </header>
 
             <Router>
-                <Home path="/"></Home>
-                <Conference path="/conference/:slug"></Conference>
+                <Home path="/" conferences={conferences}></Home>
+                <Conference path="/conference/:slug" conferences={conferences}></Conference>
             </Router>
         </div>
     )
