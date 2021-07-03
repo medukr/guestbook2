52507176a6f6d83753ab0cbc222b7f93eaaca251
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-07-03 17:53:06 +0300

    27.1

---
+++ spa/webpack.config.js
@@ -0,0 +1,12 @@
+const Encore = require('@symfony/webpack-encore');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+Encore.setOutputPath('public/')
+    .setPublicPath('/')
+    .cleanupOutputBeforeBuild()
+    .addEntry('app', './src/app.js')
+    .enablePreactPreset()
+    .enableSingleRuntimeChunk()
+    .addPlugin(new HtmlWebpackPlugin({ template: 'src/index.js', alwaysWriteToDisk: true }));
+
+module.exports = Encore.getWebpackConfig();
